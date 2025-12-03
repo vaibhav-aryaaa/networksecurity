fd624f0064856c1ce70e05ccf0e210a85d12bb09
Vaibhav Arya <vaibhavarya338@gmail.com>
2025-12-03 20:03:03 +0530

    add:Data validation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
+from networksecurity.components.data_transformation import DataTransformation
 import sys
 
 if __name__=='__main__':
@@ -21,6 +22,12 @@ if __name__=='__main__':
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data validation completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        logging.info(f"initiated Data transformation ")
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info(f"Data transformation completed")
+        print(data_transformation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)  
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -47,7 +47,7 @@ class DataValidationConfig:
                                                       training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
                                                       training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
 
-class DataTransformationConifg:
+class DataTransformationConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_transformation_dir: str=os.path.join(training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME)
         self.transformed_train_file_path: str=os.path.join(self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,training_pipeline.TRAIN_FILE_NAME.replace("csv","npy"))
