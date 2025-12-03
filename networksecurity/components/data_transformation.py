e90347f4c5125421b3b6188352239b243a9f9baa
Vaibhav Arya <vaibhavarya338@gmail.com>
2025-12-03 14:34:04 +0530

    add:data transformation config

---
+++ networksecurity/components/data_transformation.py
@@ -0,0 +1,15 @@
+import os,sys
+import numpy
+import pandas as pd
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+
+from networksecurity.constant.training_pipeline import TARGET_COLUMN
+from networksecurity.constant.training_pipeline import DATA_TRANSFORMATION_IMPUTER_PARAMS
+
+from networksecurity.entity.artifact_entity import DataTransformationArtifact,DataValidationArtifact
+
+from networksecurity.entity.config_entity import DataTransformationConifg
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.utils.main_utils.utils import save_numpy_array_data,save_object
