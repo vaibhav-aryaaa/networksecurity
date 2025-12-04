8aff091d40adadc81a83683b326ac73e75c55b28
Vaibhav Arya <vaibhavarya338@gmail.com>
2025-12-04 14:19:17 +0530

    add:TrainerEvaluation and MLFlow setup

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,4 +1,5 @@
 import os,sys
+import mlflow
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
@@ -29,6 +30,18 @@ class ModelTrainer:
             
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+            
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision_score",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            
+            mlflow.sklearn.log_model(best_model,"model")
         
     def train_model(self,x_train,y_train,x_test,y_test):
         models={
@@ -77,8 +90,8 @@ class ModelTrainer:
         
         classification_train_metric=get_classfication_score(y_true=y_train,y_pred=y_train_pred)
         
-        # Track the MLFlow
-        
+        # Track the experiments with MLFlow
+        self.track_mlflow(best_model,classification_train_metric)
         
         
         y_test_pred=best_model.predict(x_test)
